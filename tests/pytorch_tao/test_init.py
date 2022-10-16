d6fc0b00ab9b7a9bae75285753e4aaf132cba9f8
chenglu <chenglu.she@gmail.com>
2022-10-16 22:34:25 +0800

    add test conv report

--- tests/pytorch_tao/test_init.py
+++ tests/pytorch_tao/test_init.py
@@ -1,5 +1,17 @@
-import pytorch_tao
+import pytorch_tao as tao
 
 
 def test_version():
-    assert pytorch_tao.__version__ is not None
+    assert tao.__version__ is not None
+
+
+def test_global_attr_are_set():
+    assert hasattr(tao, "args")
+    assert hasattr(tao, "study")
+    assert hasattr(tao, "trial")
+    assert hasattr(tao, "cfg")
+    assert hasattr(tao, "repo")
+    assert hasattr(tao, "tracker")
+    assert hasattr(tao, "log_dir")
+    assert hasattr(tao, "name")
+    assert hasattr(tao, "tune")
